ce821358876f63b788f2a58ceeec76cb98e95828
sanuvish11 <shanoo@alinasoftwares.in>
2021-12-20 18:13:53 +0530

    change done

--- controller/countryCtrl.js
+++ controller/countryCtrl.js
@@ -25,49 +25,5 @@ router.get('/list', async (req, res, next) => {
         })
     }
 })
-//Country update
-router.post('/update/:id', async (req, res, next) => {
-    try {
-        const countryUpdate = await db.countryModel.update(req.body, { id: req.params.id })
-        if (countryUpdate) {
-            return res.send({
-                status: 200,
-                results: messageConst.updateSuccuss
-            })
-        }
-        return res.send({
-            status: 404,
-            results: messageConst.listblank
-        })
-    } catch (error) {
-        console.log(error)
-        return res.status(500).send({
-            status: 500,
-            message: messageConst.unableProcess
-        })
-    }
-})
-//delete category
-router.delete('/delete/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const country = await db.countryModel.destroy({ where: { id: id } })
-        if (!country) {
-            return res.json({
-                status: 404,
-                message: messageConst.deleteError
-            });
-        }
-        return res.send({
-            status: 200,
-            category: messageConst.deleteSucess
-        })
-    } catch (error) {
-        return res.send({
-            status: 500,
-            message: messageConst.unableProcess
-        })
-    }
 
-})
 module.exports = router;
--- controller/nationalityCtls.js
+++ controller/nationalityCtls.js
@@ -2,57 +2,7 @@ var express = require('express');
 var router = express.Router();
 const db = require("../models");
 const messageConst = require('../config/message');
-//create nationality
-router.post('/save', async (req, res, next) => {
-    try {
-        const nationality = await db.nationalityModel.findOne({ where: { name: req.body.name } })
-        if (nationality) {
-            return res.send({
-                status: 208,
-                message: messageConst.nationalityExist
-            })
-        }
-        const categoryCreate = await db.categoryModel.create({
-            name: req.body.name,
-            status: req.body.status,
-        })
-        if (categoryCreate) {
-            return res.send({
-                status: 200,
-                message: messageConst.nationalityCreateSuccess
-            })
-        }
 
-    } catch (error) {
-        console.log(error)
-        return res.status(500).send({
-            status: 500,
-            message: messageConst.unableProcess
-        })
-    }
-})
-//update category
-router.post('/update/:id', async (req, res, next) => {
-    try {
-        const nationalityUpdate = await db.nationalityModel.update(req.body, { where: { id: req.params.id } })
-        if (nationalityUpdate) {
-            return res.send({
-                status: 200,
-                message: messageConst.updateSuccuss
-            })
-        }
-        return res.send({
-            status: 204,
-            message: messageConst.NoContent
-        });
-    } catch (error) {
-        console.log(error)
-        return res.status(500).send({
-            status: 500,
-            message: messageConst.unableProcess
-        })
-    }
-})
 //user list
 router.get('/list', async (req, res, next) => {
     try {
@@ -75,27 +25,5 @@ router.get('/list', async (req, res, next) => {
         })
     }
 })
-//delete category
-router.delete('/delete/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const nationality = await db.nationalityModel.destroy({ where: { id: id } })
-        if (!nationality) {
-            return res.json({
-                status: 404,
-                message: messageConst.deleteError
-            });
-        }
-        return res.send({
-            status: 200,
-            message: messageConst.deleteSucess
-        })
-    } catch (error) {
-        return res.send({
-            status: 500,
-            message: messageConst.unableProcess
-        })
-    }
 
-})
 module.exports = router;
